bb3283fd1264c30421265faa34a886754d584b5d
senami77 <cloudcat0v0@gmail.com>
2025-07-11 07:36:39 +0900

    Add files via upload

---
+++ 0710.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int radius = 5;
+
+	float Pi = 3.14;
+
+	float area = radius * radius * Pi; 
+
+	float girth = radius * 2*Pi;
+
+	printf("원의 넓이:%.2f", area);
+	printf("원의 둘레:%.2f", girth);
+
+	return 0;
+}
