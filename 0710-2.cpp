3a82a1835ab363c4d17144553241065414782569
senami77 <cloudcat0v0@gmail.com>
2025-07-10 16:53:37 +0900

    Add files via upload

---
+++ 0710-2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int radius = 5;
+
+	float Pi = 3.14;
+
+	float area = radius*radius* Pi; //그냥 계산식을 다 적기
+
+	float girth = radius * 2;
+
+	printf("원의 넓이:%.2f", area); //출력만 하기
+
+
+
+	return 0;
+}
---
+++ 0710.cpp
@@ -0,0 +1,45 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+	printf("사과 1개\n");
+	return 0;
+}*/
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	int apple = 1;
+	printf("사과 %d개\n", apple);
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+short main()
+{
+	short coin1 = 500;
+	short coin2 = 1000;
+	printf("%d+%d=%d\n",  coin1, coin2, coin1+coin2);
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	char A;
+	A = 'A';
+	printf("A=%c, A=%d\n",A,A);
+	return 0;
+}
+*/
+
