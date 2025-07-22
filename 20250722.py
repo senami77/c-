398bf86ffe18b4fcfaf29751f1345aef47992cc1
senami77 <cloudcat0v0@gmail.com>
2025-07-22 16:50:42 +0900

    Add files via upload

--- 20250722.py
+++ 20250722.py
@@ -29,3 +29,91 @@ print("\\n은 줄바꿈의 대체 기호입니다.") #\\가 두 개씩 있으면
 print("따옴표는 '역할'이 같아요")
 print('따옴표는 "역할"이 같아요')
 '''
+
+'''
+#실행불가
+print('따옴표는 "역할'이 같아요")
+print('따옴표는 "역할"이 같아요")
+'''
+
+'''
+apple = "사과"
+print(apple,1,"상자")
+
+apple = "사과"
+print(apple,1,"상자",sep="++")
+
+print("이샘")
+print("코딩")
+print("학원")
+
+print("이샘",end="")
+print("코딩",end="")
+print("학원",end="!")
+'''
+
+'''
+num1 = 10
+print(num1, type(num1))
+
+num2 = 3.14
+print(num2, type(num2))
+
+m_str = "안녕하세요"
+print(m_str, type(m_str))
+
+print(1+1, type(1+1))
+
+print(0.3+2.04, type(0.3+2.04))
+
+print(1+2.3, type(1+2.3))
+'''
+
+'''
+#문제1)
+print("\"이샘",end="")
+print("코딩",end="")
+print("학원",end="\"")
+
+#문제2)
+print(1,2,3,sep="+",end="=6")
+'''
+
+'''
+num1=17
+print(num1)
+print("10진수 17 = %d"%(num1))
+print("10진수 17의 8진수 = %o"%(num1))
+print("10진수 17의 16진수 = %x"%(num1))
+
+num2 = 97
+print("%d %c" % (num2,num2)) #문자 <-유니코드 표에 적힌 숫자 번호로 출력
+
+float1=1234.567
+print(float1)
+print("기본 출력 %f"%(float1))
+print("지수표기법 %e"%(float1))
+print("소수 둘째자리까지%.2f"%(float1))
+
+txt1="Hello"
+txt2="World!"
+
+print("기본 출력\n%s %s"%(txt1,txt2))
+print("7칸 출력\n%7s %7s"%(txt1,txt2))
+'''
+
+num1=17
+print(num1)
+print("10진수 17 = {0}".format(num1))
+print("10진수 17의 8진수 = {0:o}".format(num1))
+print("10진수 17의 16진수 = {0:x}".format(num1))
+
+num2=98
+print("{0} {1:c}".format(num2,num2))
+
+
+
+
+
+
+
--- 20250722shell.py
+++ 20250722shell.py
@@ -1,26 +1,26 @@
 Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> a=6
->>> a
+a=6
+a
 6
->>> 
+
 ============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
 Hello World
->>> 
+
 ============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
 이샘 코딩 전문 학원
 코딩세계에 오신걸 환영합니다
->>> 
+
 ============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
 이샘코딩전문학원
 이샘코딩학원!!이샘 코딩 전문 학원
 코딩세계에 오신걸 환영합니다
->>> 
+
 ============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
 이샘코딩전문학원
 이샘코딩학원이샘 코딩 전문 학원
 코딩세계에 오신걸 환영합니다
->>> 
+
 ============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
 안녕하세요
 안녕하세요
@@ -32,3 +32,127 @@ Hello World
 \n은 줄바꿈의 대체 기호입니다.
 따옴표는 '역할'이 같아요
 따옴표는 "역할"이 같아요
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+사과++1++상자
+이샘
+코딩
+학원
+이샘코딩학원!
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+사과 1 상자
+사과++1++상자
+이샘
+코딩
+학원
+이샘코딩학원!
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+10 <class 'int'>
+3.14 <class 'float'>
+Traceback (most recent call last):
+  File "C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py", line 62, in <module>
+    print(m_Str, type(m_str))
+NameError: name 'm_Str' is not defined. Did you mean: 'm_str'?
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+10 <class 'int'>
+3.14 <class 'float'>
+안녕하세요 <class 'str'>
+2 <class 'int'>
+2.34 <class 'float'>
+3.3 <class 'float'>
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+이샘코딩학원1+2+3=6
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+"이샘코딩학원"1+2+3=6
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+"이샘코딩학원"----------
+1+2+3=6
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
+97 a
+1234.567
+기본 출력 1234.567000
+지수표기법 1.234567e+03
+소수 둘째자리까지 1234.567000
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
+97 a
+1234.567
+기본 출력 1234.567000
+지수표기법 1.234567e+03
+소수 둘째자리까지 1234.57
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
+97 a
+1234.567
+기본 출력 1234.567000
+지수표기법 1.234567e+03
+Traceback (most recent call last):
+  File "C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py", line 95, in <module>
+    print("20250722shell.py2f"%(float1))
+TypeError: not all arguments converted during string formatting
+
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
+97 a
+1234.567
+기본 출력 1234.567000
+지수표기법 1.234567e+03
+Traceback (most recent call last):
+  File "C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py", line 95, in <module>
+    print("20250722shell.py.2f"%(float1))
+TypeError: not all arguments converted during string formatting
+>>> 
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
+97 a
+1234.567
+기본 출력 1234.567000
+지수표기법 1.234567e+03
+소수 둘째자리까지1234.57
+기본 출력
+Hello World!
+7칸 출력
+  Hello  World!
+>>> 
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 진수 = 21
+10진수 17의 16진수 = 11
+>>> 
+============ RESTART: C:/Users/sw-105/Desktop/수강생605/김서연/20250722.py ===========
+17
+10진수 17 = 17
+10진수 17의 8진수 = 21
+10진수 17의 16진수 = 11
